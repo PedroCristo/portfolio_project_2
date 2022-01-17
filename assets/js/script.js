b761bf4ae32a29049deadc4283ca6f68397a761e
Pedro Cristo <pcristo35@gmail.com>
2022-01-17 10:26:34 +0000

    Add JS code for the menu day/night mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,27 @@
      closeMenu.classList.toggle('active');
      menuLeft.classList.toggle('active');
  });
+
+
+/**
+ * Change background color (onload) menu day/night mode
+ */
+ const dt = new Date();
+ const th = dt.getHours();
+ const faMoon = document.querySelector('.fa-moon');
+ const faSun = document.querySelector('.fa-sun');
+ const body =  document.getElementById('body');
+ 
+  function changeColorBg() {
+      if(th >= 8 && th <= 19) {
+
+        body.classList.remove('active');
+        faMoon.classList.add('active');
+        
+    } else {
+        
+        body.classList.add('active');
+        faSun.classList.add('active');
+
+      }
+  }
