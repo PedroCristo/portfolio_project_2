ef8ae5fe757f47ccc8d32af507f2ad3222ac03c3
Pedro Cristo <pcristo35@gmail.com>
2022-01-18 11:24:24 +0000

    Add  map score method and statements in the scores js

--- assets/js/score.js
+++ assets/js/score.js
@@ -7,3 +7,28 @@
  */
   const highScoresList = document.getElementById('highScoresList');
   const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+  /**
+ * Show to the users the highscores board returning a list with username, score and medal if the user get a score over 199
+ */
+highScoresList.innerHTML = highScores
+.map(score => {
+  if (score.score == 250) {
+
+    return `<li class="high-score">${score.name} - ${score.score} - <i class="fas fa-medal gold-color"></i></li>`;
+    
+  } else if (score.score == 225) {
+
+    return `<li class="high-score">${score.name} - ${score.score} - <i class="fas fa-medal silver-color"></i></li>`;
+
+  } else if (score.score == 200) {
+    
+    return `<li class="high-score">${score.name} - ${score.score} - <i class="fas fa-medal bronze-color"></i></li>`;
+
+  } else {
+
+    return `<li class="high-score">${score.name} - ${score.score}`;
+
+  }
+})
+.join("");
