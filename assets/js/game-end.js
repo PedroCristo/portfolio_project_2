2049608d0884881136e600f6829c6a6ce8c271db
Pedro Cristo <pcristo35@gmail.com>
2022-01-19 10:13:58 +0000

    Add media querie for (max-width:740px) & (max-height:360px)

--- assets/js/game-end.js
+++ assets/js/game-end.js
@@ -7,7 +7,6 @@
  * Declare const for DOM elements 
  */
  const username = document.getElementById('username');
-//  const saveScore = document.getElementById('save-score');
  const finalScore = document.getElementById('final-score');
  const lastScore = localStorage.getItem('lastScore');
  
