5e609cbd7b305179e41fb856a65af49cc6cb88db
Pedro Cristo <pcristo35@gmail.com>
2022-01-29 13:47:03 +0000

    Change comments and remove extra spaces in the code

--- assets/js/game-end.js
+++ assets/js/game-end.js
@@ -4,7 +4,6 @@
  *******************************/
 
 // Declare const for DOM elements 
-
  const username = document.getElementById('username');
  const finalScore = document.getElementById('final-score');
  const lastScore = localStorage.getItem('lastScore');
@@ -13,20 +12,20 @@
  const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 
-// Enable the button when the user remove the cursor from the input
-
+ /**
+  * Enable the button when the user remove the cursor from the input
+  * */ 
  username.addEventListener('keyup', () => {
     saveScore.disabled = !username.value;
 });
 
 
 // Show to the users the last score 
- 
  finalScore.innerText = lastScore;
 
-
-// Save users last score and username
- 
+/**
+ * Save users last score and username
+ * */
  saveHighScore = (e) => {
     e.preventDefault();
 
@@ -38,9 +37,7 @@
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(6);
 
-    
     // Brings the user to the right page depends on the score made by the user in the last game played
-    
      if (lastScore == GOLD_TROPHY_SCORE) {
 
         window.location.assign('trophy-gold.html');
