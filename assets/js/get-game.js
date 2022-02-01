ac9eca1b4b0118191d55dc3b5fa8cc6e24971d6c
Pedro Cristo <pcristo35@gmail.com>
2022-02-01 12:34:33 +0000

    Change quizPageArray

--- assets/js/get-game.js
+++ assets/js/get-game.js
@@ -10,7 +10,9 @@
  * */
  getQuizBtn.addEventListener('click', () => {
    
-    let quizPageArray = ['pages/game-1.html', 'pages/game-2.html', 'pages/game-3.html'];
+    let quizPageArray = ['pages/game-1.html', 
+                         'pages/game-2.html', 
+                         'pages/game-3.html'];
     const QUIZ_PAGE_ARRAY_INDEX = Math.floor(Math.random() * quizPageArray.length);
     quizPageArray = quizPageArray[QUIZ_PAGE_ARRAY_INDEX];
   
