af470b5cb5e0d7a0db3a956406eefde3e3ea7fdc
Pedro Cristo <pcristo35@gmail.com>
2022-02-01 12:32:35 +0000

    Change and add comment for variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,13 +2,14 @@
  ********** GAME ***************
  *******************************/
 
-// Declare const and variables for DOM elements 
+// Declare const for DOM elements 
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progress-text');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progress-bar-full');
 
+// Declare variables for game
 let currentQuestion = {};
 let score = 0;
 let questionCounter = 0;
